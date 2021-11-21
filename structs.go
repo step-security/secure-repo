ea427bc7cae72e637c53848623739f9d7d0b0568
Varun Sharma <varunsh@stepsecurity.io>
2021-11-20 18:37:37 -0800

    Remove json tags

--- structs.go
+++ structs.go
@@ -25,22 +25,22 @@ type Env map[string]string
 // For action-permissions.yml
 
 type Permissions struct {
-	Actions        string `yaml:"actions" json:"actions"`
-	Checks         string `yaml:"checks" json:"checks"`
-	Contents       string `yaml:"contents" json:"contents"`
-	Deployments    string `yaml:"deployments" json:"deployments"`
-	Packages       string `yaml:"packages" json:"packages"`
-	PullRequests   string `yaml:"pull-requests" json:"pull-requests"`
-	Issues         string `yaml:"issues" json:"issues"`
-	SecurityEvents string `yaml:"security-events" json:"security-events"`
-	Statuses       string `yaml:"statuses" json:"statuses"`
+	Actions        string `yaml:"actions"`
+	Checks         string `yaml:"checks"`
+	Contents       string `yaml:"contents"`
+	Deployments    string `yaml:"deployments"`
+	Packages       string `yaml:"packages"`
+	PullRequests   string `yaml:"pull-requests"`
+	Issues         string `yaml:"issues"`
+	SecurityEvents string `yaml:"security-events"`
+	Statuses       string `yaml:"statuses"`
 }
 
 type Action struct {
-	Name         string      `yaml:"name" json:"name"`
-	DefaultToken string      `yaml:"default-token" json:"default-token"`
-	EnvKey       string      `yaml:"env-key" json:"env-key"`
-	Permissions  Permissions `yaml:"permissions" json:"permissions"`
+	Name         string      `yaml:"name"`
+	DefaultToken string      `yaml:"default-token"`
+	EnvKey       string      `yaml:"env-key"`
+	Permissions  Permissions `yaml:"permissions"`
 }
 
 type ActionPermissions struct {
