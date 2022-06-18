e4e1124774b4d6e81e399820c89b1f503dc36ffd
arjundashrath <54043589+arjundashrath@users.noreply.github.com>
2022-06-18 13:49:31 +0530

    Update permissions_test.go

--- permissions_test.go
+++ permissions_test.go
@@ -133,3 +133,51 @@ func TestAddWorkflowLevelPermissions(t *testing.T) {
 	}
 
 }
+
+func TestReusableWorkflow(t *testing.T) {
+	const inputDirectory = "./testfiles/reusableWorkflow"
+	files, err := ioutil.ReadDir(inputDirectory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		input, err := ioutil.ReadFile(path.Join(inputDirectory, f.Name()))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fixWorkflowPermsResponse, err := AddJobLevelPermissions(string(input))
+		output := fixWorkflowPermsResponse.FinalOutput
+		jobErrors := fixWorkflowPermsResponse.JobErrors
+
+		// some test cases return a job error for known issues
+		if len(jobErrors) > 0 {
+			for _, je := range jobErrors {
+				if strings.Compare(je.JobName, "job-with-error") == 0 {
+					if strings.Contains(je.Errors[0], "KnownIssue") {
+						output = je.Errors[0]
+					} else {
+						t.Errorf("test failed. unexpected job error %s, error: %v", f.Name(), jobErrors)
+					}
+				}
+			}
+
+		}
+
+		if fixWorkflowPermsResponse.AlreadyHasPermissions {
+			output = errorAlreadyHasPermissions
+		}
+
+		if fixWorkflowPermsResponse.IncorrectYaml {
+			output = errorIncorrectYaml
+		}
+
+		if !strings.Contains(jobErrors[0].Errors[0], "KnownIssue-7") {
+			t.Errorf("test failed %s, error: %v", f.Name(), jobErrors[0].Errors[0])
+		}
+
+		_ = output
+	}
+}
