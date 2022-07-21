86de2b132bd7b4a3be59e6d61760fdf90361d4a0
h0x0er <jatink843@protonmail.com>
2022-07-21 23:56:30 +0530

    minor changes in tests

--- securedockerfile_test.go
+++ securedockerfile_test.go
@@ -9,6 +9,8 @@ import (
 	"github.com/jarcoal/httpmock"
 )
 
+var resp = httpmock.File("./testfiles/dockerfiles/response.json").String()
+
 func TestSecureDockerFile(t *testing.T) {
 
 	const inputDirectory = "./testfiles/dockerfiles/input"
@@ -35,62 +37,7 @@ func TestSecureDockerFile(t *testing.T) {
 		httpmock.NewStringResponder(200, `{
 	}`))
 
-	httpmock.RegisterResponder("GET", "https://index.docker.io/v2/library/python/manifests/3.7", httpmock.NewStringResponder(200, `{
-	// 	"schemaVersion": 2,
-	// 	"mediaType": "application/vnd.docker.distribution.manifest.v2+json",
-	// 	"config": {
-	// 	   "mediaType": "application/vnd.docker.container.image.v1+json",
-	// 	   "size": 9201,
-	// 	   "digest": "sha256:142b92ebe662c93bcf18cfe0c3174a76b4a7e62f290a51828aaa9eb630569008"
-	// 	},
-	// 	"layers": [
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 54999406,
-	// 		  "digest": "sha256:d836772a1c1f9c4b1f280fb2a98ace30a4c4c87370f89aa092b35dfd9556278a"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 5156110,
-	// 		  "digest": "sha256:66a9e63c657ad881997f5165c0826be395bfc064415876b9fbaae74bcb5dc721"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 10876416,
-	// 		  "digest": "sha256:d1989b6e74cfdda1591b9dd23be47c5caeb002b7a151379361ec0c3f0e6d0e52"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 54579006,
-	// 		  "digest": "sha256:c28818711e1ed38df107014a20127b41491b224d7aed8aa7066b55552d9600d2"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 196774352,
-	// 		  "digest": "sha256:5084fa7ebd744165b15df008a9c14db7fc3d6af34cce64ba85bbaa348af594a3"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 6290793,
-	// 		  "digest": "sha256:7f162c881e4f4673d8c7093e578c2dff9d249bc73fe24db52f9eb05c0575fa38"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 15481270,
-	// 		  "digest": "sha256:fa5c3534431da089d8d042efd57be7f00a6e9f9b0fc536bd4b1120f146d26f15"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 234,
-	// 		  "digest": "sha256:30596f5e8e78d938757214af98c9ddede83f1f8a3e10e55145f8c643d09f399d"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 2874571,
-	// 		  "digest": "sha256:4861fe1f6551067ce2f2ae0b1201b3bacc356985cc986eab5c00b441c193fa10"
-	// 	   }
-	// 	]
-	//  }`))
+	httpmock.RegisterResponder("GET", "https://index.docker.io/v2/library/python/manifests/3.7", httpmock.NewStringResponder(200, resp))
 
 	tests := []struct {
 		fileName  string
@@ -152,62 +99,7 @@ func Test_getSHA(t *testing.T) {
 		httpmock.NewStringResponder(200, `{
 	}`))
 
-	httpmock.RegisterResponder("GET", "https://index.docker.io/v2/library/python/manifests/3.7", httpmock.NewStringResponder(200, `{
-	// 	"schemaVersion": 2,
-	// 	"mediaType": "application/vnd.docker.distribution.manifest.v2+json",
-	// 	"config": {
-	// 	   "mediaType": "application/vnd.docker.container.image.v1+json",
-	// 	   "size": 9201,
-	// 	   "digest": "sha256:142b92ebe662c93bcf18cfe0c3174a76b4a7e62f290a51828aaa9eb630569008"
-	// 	},
-	// 	"layers": [
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 54999406,
-	// 		  "digest": "sha256:d836772a1c1f9c4b1f280fb2a98ace30a4c4c87370f89aa092b35dfd9556278a"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 5156110,
-	// 		  "digest": "sha256:66a9e63c657ad881997f5165c0826be395bfc064415876b9fbaae74bcb5dc721"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 10876416,
-	// 		  "digest": "sha256:d1989b6e74cfdda1591b9dd23be47c5caeb002b7a151379361ec0c3f0e6d0e52"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 54579006,
-	// 		  "digest": "sha256:c28818711e1ed38df107014a20127b41491b224d7aed8aa7066b55552d9600d2"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 196774352,
-	// 		  "digest": "sha256:5084fa7ebd744165b15df008a9c14db7fc3d6af34cce64ba85bbaa348af594a3"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 6290793,
-	// 		  "digest": "sha256:7f162c881e4f4673d8c7093e578c2dff9d249bc73fe24db52f9eb05c0575fa38"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 15481270,
-	// 		  "digest": "sha256:fa5c3534431da089d8d042efd57be7f00a6e9f9b0fc536bd4b1120f146d26f15"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 234,
-	// 		  "digest": "sha256:30596f5e8e78d938757214af98c9ddede83f1f8a3e10e55145f8c643d09f399d"
-	// 	   },
-	// 	   {
-	// 		  "mediaType": "application/vnd.docker.image.rootfs.diff.tar.gzip",
-	// 		  "size": 2874571,
-	// 		  "digest": "sha256:4861fe1f6551067ce2f2ae0b1201b3bacc356985cc986eab5c00b441c193fa10"
-	// 	   }
-	// 	]
-	//  }`))
+	httpmock.RegisterResponder("GET", "https://index.docker.io/v2/library/python/manifests/3.7", httpmock.NewStringResponder(200, resp))
 
 	type args struct {
 		image string
@@ -220,7 +112,7 @@ func Test_getSHA(t *testing.T) {
 		wantErr bool
 	}{
 		// TODO: Add test cases.
-		{name: "test1", args: args{image: "python", tag: "3.7"}, want: "sha256:500cc991c42e14eda7d4cc56508ac4331f829a4497724ed5f5f964a9e1e33367", wantErr: false},
+		{name: "test1", args: args{image: "python", tag: "3.7"}, want: "sha256:5fb6f4b9d73ddeb0e431c938bee25c69157a1e3c880a81ff72c43a8055628de5", wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
