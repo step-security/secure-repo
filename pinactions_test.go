df1ce0692d92625e217496c883740b6aa9d0ca38
h0x0er <jatink843@protonmail.com>
2022-08-23 16:25:21 +0530

    modified test cases

--- pinactions_test.go
+++ pinactions_test.go
@@ -16,113 +16,33 @@ func TestPinActions(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
-	httpmock.RegisterResponder("GET", "https://api.github.com/repos/peter-evans/close-issue/git/ref/tags/v1",
-		httpmock.NewStringResponder(200, `{
-			"ref": "refs/tags/v1",
-			"node_id": "MDM6UmVmMjYxMDY4MTU5OnJlZnMvdGFncy92MQ==",
-			"url": "https://api.github.com/repos/peter-evans/close-issue/git/refs/tags/v1",
-			"object": {
-			  "sha": "a700eac5bf2a1c7a8cb6da0c13f93ed96fd53dbe",
-			  "type": "commit",
-			  "url": "https://api.github.com/repos/peter-evans/close-issue/git/commits/a700eac5bf2a1c7a8cb6da0c13f93ed96fd53dbe"
-			}
-		  }`))
+	httpmock.RegisterResponder("GET", "https://api.github.com/repos/peter-evans/close-issue/commits/v1",
+		httpmock.NewStringResponder(200, `a700eac5bf2a1c7a8cb6da0c13f93ed96fd53dbe`))
 
-	httpmock.RegisterResponder("GET", "https://api.github.com/repos/actions/checkout/git/ref/heads/master",
-		httpmock.NewStringResponder(200, `{
-			"ref": "refs/heads/master",
-			"node_id": "MDM6UmVmMTk3ODE0NjI5OnJlZnMvaGVhZHMvbWFzdGVy",
-			"url": "https://api.github.com/repos/actions/checkout/git/refs/heads/master",
-			"object": {
-			  "sha": "61b9e3751b92087fd0b06925ba6dd6314e06f089",
-			  "type": "commit",
-			  "url": "https://api.github.com/repos/actions/checkout/git/commits/61b9e3751b92087fd0b06925ba6dd6314e06f089"
-			}
-		  }`))
+	httpmock.RegisterResponder("GET", "https://api.github.com/repos/actions/checkout/commits/master",
+		httpmock.NewStringResponder(200, `61b9e3751b92087fd0b06925ba6dd6314e06f089`))
 
-	httpmock.RegisterResponder("GET", "https://api.github.com/repos/elgohr/Publish-Docker-Github-Action/git/ref/heads/master",
-		httpmock.NewStringResponder(200, `{
-			"ref": "refs/heads/master",
-			"node_id": "MDM6UmVmMTcyMjA5NzEzOnJlZnMvaGVhZHMvbWFzdGVy",
-			"url": "https://api.github.com/repos/elgohr/Publish-Docker-Github-Action/git/refs/heads/master",
-			"object": {
-			  "sha": "49d9c2e46838527972659f80f5488d08971fdc2d",
-			  "type": "commit",
-			  "url": "https://api.github.com/repos/elgohr/Publish-Docker-Github-Action/git/commits/49d9c2e46838527972659f80f5488d08971fdc2d"
-			}
-		  }`))
+	httpmock.RegisterResponder("GET", "https://api.github.com/repos/elgohr/Publish-Docker-Github-Action/commits/master",
+		httpmock.NewStringResponder(200, `8217e91c0369a5342a4ef2d612de87492410a666`))
 
-	httpmock.RegisterResponder("GET", "https://api.github.com/repos/borales/actions-yarn/git/ref/tags/v2.3.0",
-		httpmock.NewStringResponder(200, `{
-			"ref": "refs/tags/v2.3.0",
-			"node_id": "MDM6UmVmMTcyMjA5NzEzOnJlZnMvaGVhZHMvbWFzdGVy",
-			"url": "https://api.github.com/repos/borales/actions-yarn/git/ref/tags/v2.3.0",
-			"object": {
-			  "sha": "4965e1a0f0ae9c422a9a5748ebd1fb5e097d22b9",
-			  "type": "commit",
-			  "url": "https://api.github.com/repos/borales/actions-yarn/git/commits/4965e1a0f0ae9c422a9a5748ebd1fb5e097d22b9"
-			}
-		  }`))
+	httpmock.RegisterResponder("GET", "https://api.github.com/repos/borales/actions-yarn/commits/v2.3.0",
+		httpmock.NewStringResponder(200, `4965e1a0f0ae9c422a9a5748ebd1fb5e097d22b9`))
 
-	httpmock.RegisterResponder("GET", "https://api.github.com/repos/actions/checkout/git/ref/tags/v1",
-		httpmock.NewStringResponder(200, `{
-			"ref": "refs/tags/v1",
-			"node_id": "MDM6UmVmMTk3ODE0NjI5OnJlZnMvdGFncy92MQ==",
-			"url": "https://api.github.com/repos/actions/checkout/git/refs/tags/v1",
-			"object": {
-			  "sha": "544eadc6bf3d226fd7a7a9f0dc5b5bf7ca0675b9",
-			  "type": "tag",
-			  "url": "https://api.github.com/repos/actions/checkout/git/tags/544eadc6bf3d226fd7a7a9f0dc5b5bf7ca0675b9"
-			}
-		  }`))
+	httpmock.RegisterResponder("GET", "https://api.github.com/repos/actions/checkout/commits/v1",
+		httpmock.NewStringResponder(200, `544eadc6bf3d226fd7a7a9f0dc5b5bf7ca0675b9`))
 
-	httpmock.RegisterResponder("GET", "https://api.github.com/repos/actions/setup-node/git/ref/tags/v1",
-		httpmock.NewStringResponder(200, `{
-			"ref": "refs/tags/v1",
-			"node_id": "MDM6UmVmMTg5NDc2OTA0OnJlZnMvdGFncy92MQ==",
-			"url": "https://api.github.com/repos/actions/setup-node/git/refs/tags/v1",
-			"object": {
-			  "sha": "f1f314fca9dfce2769ece7d933488f076716723e",
-			  "type": "commit",
-			  "url": "https://api.github.com/repos/actions/setup-node/git/commits/f1f314fca9dfce2769ece7d933488f076716723e"
-			}
-		  }`))
+	httpmock.RegisterResponder("GET", "https://api.github.com/repos/actions/setup-node/commits/v1",
+		httpmock.NewStringResponder(200, `f1f314fca9dfce2769ece7d933488f076716723e`))
 
-	httpmock.RegisterResponder("GET", "https://api.github.com/repos/JS-DevTools/npm-publish/git/ref/tags/v1",
-		httpmock.NewStringResponder(200, `{
-			"ref": "refs/tags/v1",
-			"node_id": "MDM6UmVmMjM1MjMxNTEzOnJlZnMvdGFncy92MQ==",
-			"url": "https://api.github.com/repos/JS-DevTools/npm-publish/git/refs/tags/v1",
-			"object": {
-			  "sha": "22595ff8c4d0d9f53cef0656fbb90fbe06ee885c",
-			  "type": "tag",
-			  "url": "https://api.github.com/repos/JS-DevTools/npm-publish/git/tags/22595ff8c4d0d9f53cef0656fbb90fbe06ee885c"
-			}
-		  }`))
+	httpmock.RegisterResponder("GET", "https://api.github.com/repos/JS-DevTools/npm-publish/commits/v1",
+		httpmock.NewStringResponder(200, `0f451a94170d1699fd50710966d48fb26194d939`))
 
-	httpmock.RegisterResponder("GET", "https://api.github.com/repos/brandedoutcast/publish-nuget/git/ref/tags/v2",
-		httpmock.NewStringResponder(200, `{
-			"ref": "refs/tags/v2",
-			"node_id": "MDM6UmVmMjI4MTk2ODk5OnJlZnMvdGFncy92Mg==",
-			"url": "https://api.github.com/repos/brandedoutcast/publish-nuget/git/refs/tags/v2",
-			"object": {
-			  "sha": "c12b8546b67672ee38ac87bea491ac94a587f7cc",
-			  "type": "commit",
-			  "url": "https://api.github.com/repos/brandedoutcast/publish-nuget/git/commits/c12b8546b67672ee38ac87bea491ac94a587f7cc"
-			}
-		  }`))
+	httpmock.RegisterResponder("GET", "https://api.github.com/repos/brandedoutcast/publish-nuget/commits/v2",
+		httpmock.NewStringResponder(200, `c12b8546b67672ee38ac87bea491ac94a587f7cc`))
 
-	httpmock.RegisterResponder("GET", "https://api.github.com/repos/rohith/publish-nuget/git/ref/tags/v2",
-		httpmock.NewStringResponder(200, `{
-			"ref": "refs/tags/v2",
-			"node_id": "MDM6UmVmMjI4MTk2ODk5OnJlZnMvdGFncy92Mg==",
-			"url": "https://api.github.com/repos/brandedoutcast/publish-nuget/git/refs/tags/v2",
-			"object": {
-			  "sha": "c12b8546b67672ee38ac87bea491ac94a587f7cc",
-			  "type": "commit",
-			  "url": "https://api.github.com/repos/brandedoutcast/publish-nuget/git/commits/c12b8546b67672ee38ac87bea491ac94a587f7cc"
-			}
-		  }`))
+	httpmock.RegisterResponder("GET", "https://api.github.com/repos/rohith/publish-nuget/commits/v2",
+		httpmock.NewStringResponder(200, `c12b8546b67672ee38ac87bea491ac94a587f7cc`))
+
 	tests := []struct {
 		fileName    string
 		wantUpdated bool
@@ -159,5 +79,27 @@ func TestPinActions(t *testing.T) {
 		if output != string(expectedOutput) {
 			t.Errorf("test failed %s did not match expected output\n%s", tt.fileName, output)
 		}
+	}
+}
+
+func Test_isAbsolute(t *testing.T) {
+	type args struct {
+		ref string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{name: "Fixed Action", args: args{ref: "actions/setup-node@f1f314fca9dfce2769ece7d933488f076716723e"}, want: true},
+		{name: "Unfixed Action", args: args{ref: "actions/setup-node@v1"}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isAbsolute(tt.args.ref); got != tt.want {
+				t.Errorf("isAbsolute() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
