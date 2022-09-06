33cb6fb44726dfe722d7c2be7add298068e59328
Devils-Knight <shubhammalik24619@gmail.com>
2022-09-06 13:46:36 +0530

    fixed indentation and added comment

--- configdependabotfile.go
+++ configdependabotfile.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"strings"
+
 	dependabot "github.com/paulvollmer/dependabot-config-go"
+	"gopkg.in/yaml.v2"
 )
 
 type configDependabotResponse struct {
@@ -33,14 +37,37 @@ func configDependabot(configDependabotFile string) (*configDependabotResponse, e
 		schedule.Interval = "daily"
 
 		item.Schedule = schedule
-		configMetadata.AddUpdate(item)
-		data, err := configMetadata.Marshal()
+		items := []dependabot.Update{}
+		items = append(items, item)
+		addedItem, err := yaml.Marshal(items)
+		data := string(addedItem)
+		comment := "Workflow files stored in the \ndefault location of `.github/workflows`"
+
+		data = addIndentAndComment(data, comment)
 		if err != nil {
 			return nil, err
 		}
-		response.FinalOutput = string(data)
+
+		response.FinalOutput = response.FinalOutput + "\n" + data
 		response.IsChanged = true
 	}
 
 	return response, nil
+}
+
+func addIndentAndComment(data string, comment string) string {
+	scanner := bufio.NewScanner(strings.NewReader(data))
+	cnt := 0
+	finalData := "\n"
+	for scanner.Scan() {
+		if cnt == 1 {
+			scanner2 := bufio.NewScanner(strings.NewReader(comment))
+			for scanner2.Scan() {
+				finalData += "    # " + scanner2.Text() + "\n"
+			}
+		}
+		finalData += "  " + scanner.Text() + "\n"
+		cnt++
+	}
+	return finalData
 }
