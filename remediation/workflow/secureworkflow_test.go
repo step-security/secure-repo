8e7b11c4441dbaca5bff0d51e0fa83699f95af17
Varun Sharma <varunsh@stepsecurity.io>
2022-11-21 07:00:53 -0800

    Update test cases

--- remediation/workflow/secureworkflow_test.go
+++ remediation/workflow/secureworkflow_test.go
@@ -20,11 +20,92 @@ func TestSecureWorkflow(t *testing.T) {
 	httpmock.RegisterResponder("GET", "https://api.github.com/repos/actions/checkout/commits/v1",
 		httpmock.NewStringResponder(200, `544eadc6bf3d226fd7a7a9f0dc5b5bf7ca0675b9`))
 
+	httpmock.RegisterResponder("GET", "https://api.github.com/repos/actions/checkout/git/matching-refs/tags/v1.",
+		httpmock.NewStringResponder(200,
+			`[
+				{
+				  "ref": "refs/tags/v1.0.0",
+				  "node_id": "MDM6UmVmMTk3ODE0NjI5OnJlZnMvdGFncy92MS4wLjA=",
+				  "url": "https://api.github.com/repos/actions/checkout/git/refs/tags/v1.0.0",
+				  "object": {
+					"sha": "af513c7a016048ae468971c52ed77d9562c7c819",
+					"type": "commit",
+					"url": "https://api.github.com/repos/actions/checkout/git/commits/af513c7a016048ae468971c52ed77d9562c7c819"
+				  }
+				},
+				{
+				  "ref": "refs/tags/v1.1.0",
+				  "node_id": "MDM6UmVmMTk3ODE0NjI5OnJlZnMvdGFncy92MS4xLjA=",
+				  "url": "https://api.github.com/repos/actions/checkout/git/refs/tags/v1.1.0",
+				  "object": {
+					"sha": "ec3afacf7f605c9fc12c70bc1c9e1708ddb99eca",
+					"type": "tag",
+					"url": "https://api.github.com/repos/actions/checkout/git/tags/ec3afacf7f605c9fc12c70bc1c9e1708ddb99eca"
+				  }
+				},
+				{
+				  "ref": "refs/tags/v1.2.0",
+				  "node_id": "MDM6UmVmMTk3ODE0NjI5OnJlZnMvdGFncy92MS4yLjA=",
+				  "url": "https://api.github.com/repos/actions/checkout/git/refs/tags/v1.2.0",
+				  "object": {
+					"sha": "a2ca40438991a1ab62db1b7cad0fd4e36a2ac254",
+					"type": "tag",
+					"url": "https://api.github.com/repos/actions/checkout/git/tags/a2ca40438991a1ab62db1b7cad0fd4e36a2ac254"
+				  }
+				}
+			  ]`),
+	)
+
+	httpmock.RegisterResponder("GET", "https://api.github.com/repos/actions/checkout/commits/v1.2.0",
+		httpmock.NewStringResponder(200, `544eadc6bf3d226fd7a7a9f0dc5b5bf7ca0675b9`))
+
 	httpmock.RegisterResponder("GET", "https://api.github.com/repos/step-security/harden-runner/commits/v2",
-		httpmock.NewStringResponder(200, `7206db2ec98c5538323a6d70e51f965d55c11c87`))
+		httpmock.NewStringResponder(200, `ebacdc22ef6c2cfb85ee5ded8f2e640f4c776dd5`))
+
+	httpmock.RegisterResponder("GET", "https://api.github.com/repos/step-security/harden-runner/git/matching-refs/tags/v2.",
+		httpmock.NewStringResponder(200,
+			`[
+				{
+				  "ref": "refs/tags/v2.0.0",
+				  "node_id": "REF_kwDOGSuXyrByZWZzL3RhZ3MvdjIuMC4w",
+				  "url": "https://api.github.com/repos/step-security/harden-runner/git/refs/tags/v2.0.0",
+				  "object": {
+					"sha": "ebacdc22ef6c2cfb85ee5ded8f2e640f4c776dd5",
+					"type": "commit",
+					"url": "https://api.github.com/repos/step-security/harden-runner/git/commits/ebacdc22ef6c2cfb85ee5ded8f2e640f4c776dd5"
+				  }
+				}
+			  ]`),
+	)
 
 	httpmock.RegisterResponder("GET", "https://api.github.com/repos/github/super-linter/commits/v3",
 		httpmock.NewStringResponder(200, `34b2f8032d759425f6b42ea2e52231b33ae05401`))
+
+	httpmock.RegisterResponder("GET", "https://api.github.com/repos/github/super-linter/git/matching-refs/tags/v3.",
+		httpmock.NewStringResponder(200,
+			`[
+				{
+					"ref": "refs/tags/v3.17.0",
+					"node_id": "MDM6UmVmMjE2NTgxNTY3OnJlZnMvdGFncy92My4xNy4w",
+					"url": "https://api.github.com/repos/github/super-linter/git/refs/tags/v3.17.0",
+					"object": {
+					  "sha": "28cfebb84fd6dd9e8773b5efe5ac0f8f3714f228",
+					  "type": "commit",
+					  "url": "https://api.github.com/repos/github/super-linter/git/commits/28cfebb84fd6dd9e8773b5efe5ac0f8f3714f228"
+					}
+				  },
+				  {
+					"ref": "refs/tags/v3.17.1",
+					"node_id": "MDM6UmVmMjE2NTgxNTY3OnJlZnMvdGFncy92My4xNy4x",
+					"url": "https://api.github.com/repos/github/super-linter/git/refs/tags/v3.17.1",
+					"object": {
+					  "sha": "34b2f8032d759425f6b42ea2e52231b33ae05401",
+					  "type": "commit",
+					  "url": "https://api.github.com/repos/github/super-linter/git/commits/34b2f8032d759425f6b42ea2e52231b33ae05401"
+					}
+				  }
+			  ]`),
+	)
 
 	tests := []struct {
 		fileName              string
