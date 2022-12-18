d666fc708809b1113891ce92b53a8512cf1c4483
Varun Sharma <varunsh@stepsecurity.io>
2022-12-18 07:02:06 +0530

    Add support for SAST (CodeQL) workflow (#1668)

---
+++ remediation/workflow/addworkflow.go
@@ -0,0 +1,50 @@
+package workflow
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"sort"
+	"strings"
+)
+
+const CodeQLWorkflowFileName = "codeql.yml"
+
+type WorkflowParameters struct {
+	LanguagesToAdd []string
+	DefaultBranch  string
+}
+
+func getTemplate(file string) (string, error) {
+	templatePath := os.Getenv("WORKFLOW_TEMPLATES")
+
+	if templatePath == "" {
+		templatePath = "../../workflow-templates"
+	}
+
+	template, err := ioutil.ReadFile(path.Join(templatePath, file))
+	if err != nil {
+		return "", err
+	}
+
+	return string(template), nil
+}
+
+func AddWorkflow(name string, workflowParameters WorkflowParameters) (string, error) {
+	if name == "codeql" {
+		codeqlWorkflow, err := getTemplate(CodeQLWorkflowFileName)
+		if err != nil {
+			return "", err
+		}
+
+		sort.Strings(workflowParameters.LanguagesToAdd)
+		codeqlWorkflow = strings.ReplaceAll(codeqlWorkflow, "$default-branch", fmt.Sprintf(`"%s"`, workflowParameters.DefaultBranch))
+		codeqlWorkflow = strings.ReplaceAll(codeqlWorkflow, "$detected-codeql-languages", strings.Join(workflowParameters.LanguagesToAdd, ", "))
+		codeqlWorkflow = strings.ReplaceAll(codeqlWorkflow, "$cron-weekly", fmt.Sprintf(`"%s"`, "0 0 * * 1")) // Note: Runs every monday at 12:00 AM
+
+		return codeqlWorkflow, nil
+	} else {
+		return "", fmt.Errorf("match for %s Workflow name not found", name)
+	}
+}
---
+++ remediation/workflow/addworkflow_test.go
@@ -0,0 +1,54 @@
+package workflow
+
+import (
+	"io/ioutil"
+	"reflect"
+	"testing"
+)
+
+func Test_AddWorkflow(t *testing.T) {
+	tests := []struct {
+		workflowName       string
+		workflowParameters WorkflowParameters
+		expectedError      bool
+		expectedOutputFile string
+	}{
+		{
+			workflowName: "codeql",
+			workflowParameters: WorkflowParameters{
+				LanguagesToAdd: []string{"cpp", "go", "java"},
+				DefaultBranch:  "main",
+			},
+			expectedError:      false,
+			expectedOutputFile: "../../testfiles/expected-codeql.yml",
+		},
+		{
+			workflowName: "xyz",
+			workflowParameters: WorkflowParameters{
+				LanguagesToAdd: []string{"cpp"},
+				DefaultBranch:  "main",
+			},
+			expectedError:      true,
+			expectedOutputFile: "",
+		},
+	}
+
+	for _, test := range tests {
+		output, err := AddWorkflow(test.workflowName, test.workflowParameters)
+		if err != nil {
+			if !test.expectedError {
+				t.Errorf("Error adding Workflow: %v", err)
+			}
+			continue
+		}
+		expectedOutput, err := ioutil.ReadFile(test.expectedOutputFile)
+		if err != nil {
+			t.Errorf("Error in reading file: %v", err)
+		}
+
+		if !reflect.DeepEqual(output, string(expectedOutput)) {
+			t.Errorf("test failed %s did not match expected output\n%s", test.workflowName, output)
+		}
+	}
+
+}
